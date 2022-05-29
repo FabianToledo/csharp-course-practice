bfd16e45b4ebc5ee9474b2038b09b44541e76fac
Fabian Toledo <fabian.alejandro.toledo@gmail.com>
2022-05-29 13:24:18 -0300

    LINQ

---
+++ 11-HelloLinq/HelloLinq/Program.cs
@@ -0,0 +1,106 @@
+﻿using System.Collections;
+
+IEnumerable enumerable = new int[] { 1, 2, 3, 4, 5 };
+
+// The interface IEnumerable only contains one member GetEnumerator
+var enumerator = enumerable.GetEnumerator();
+
+// The interface IEnumerator contains 1 member: the current object
+// and 2 methods MoveNext and Reset
+
+while (enumerator.MoveNext())
+{
+    Console.WriteLine(enumerator.Current);
+}
+
+Console.WriteLine();
+
+enumerator.Reset();
+enumerator.MoveNext();
+Console.WriteLine(enumerator.Current);
+enumerator.MoveNext();
+Console.WriteLine(enumerator.Current);
+
+// So, IEnumerable is a read only, forward only collection
+// We cannot manipulate an IEnumerable, go back, jump, access with an index
+
+Console.WriteLine();
+/// Let us write a simple IEnumerable generator 
+//
+var even = GenerateNumbersBad(10) // The function is called at this point of the program
+    // But the where is called we the program gets to the in statement of the foreach loop
+    .Where(n => 
+    {
+        return n % 2 == 0; 
+    });
+
+foreach (var n in even)
+{
+    Console.WriteLine(n);
+}
+
+Console.WriteLine();
+var evenG = GenerateNumbersGood(10) // The function is not called at this point of the program
+    .Where(n =>
+    {
+        return n % 2 == 0;
+    })
+    .Select(n =>
+    {
+        return n * 3;
+    });
+
+foreach (var n in evenG) // the function GenerateNumbersGood is called when we get to the 'in' statement
+{
+    Console.WriteLine(n);
+}
+
+Console.WriteLine();
+Console.WriteLine($"Count: {evenG.Count()}");
+
+Console.WriteLine();
+var res = GenerateNumbersGood(10)
+    .Where(n =>
+    {
+        return n % 2 == 0;
+    })
+    // When we add an OrderBy clause, the execution is still deferred
+    // but it executes with the first MoveNext all the Where statement, all the OrderBy and one time the Select statement
+    // the next MoveNext executes only the Select
+    .OrderByDescending(n =>
+    { 
+        return n;
+    })
+    .Select(n =>
+    {
+        return n * 3;
+    });
+
+foreach (var n in res) // the function GenerateNumbersGood is called when we get to the 'in' statement
+{
+    Console.WriteLine(n);
+}
+
+/// <summary>
+/// not good code, all the numbers are generated and stored in memory all at once
+/// </summary>
+IEnumerable<int> GenerateNumbersBad(int maxValue)
+{
+    var result = new List<int>();
+    for (var i = 0; i < maxValue; i++)
+    {
+        result.Add(i);
+    }
+    return result;
+}
+
+/// <summary>
+/// A beter code, each number is generated only when it is needed.
+/// </summary>
+IEnumerable<int> GenerateNumbersGood(int maxValue)
+{
+    for (var i = 0; i < maxValue; i++)
+    {
+        yield return i;
+    }
+}
---
+++ 12-HelloLinq2/HelloLinq2/Program.cs
@@ -0,0 +1,178 @@
+﻿
+using System.Text.Json;
+using System.Text.Json.Serialization;
+
+// To generate the data, go to: https://www.mockaroo.com/
+
+// Read all the data from the file "data.json"
+// Note, to use a relative path: in properties of the file data.json choose -> Copy to output directory: copy if newer
+var fileContent = await File.ReadAllTextAsync(@"data.json");
+
+// Deserialize the content to an object
+var cars = JsonSerializer.Deserialize<List<CarData>>(fileContent);
+
+// If the list is null create an empty list
+cars ??= new List<CarData>();
+
+// Print all cars with at least 4 doors
+var carsWithAtLeast4Doors = cars.Where(c => c.NumberOfDoors >= 4);
+
+//foreach (var car in carsWithAtLeast4Doors)
+//{
+//    Console.WriteLine($"{car.Make} - {car.Model} - {car.NumberOfDoors}".PadLeft(30));
+//}
+
+// Print all Mazda cars with at least 4 doors
+var mazdasWithAtLeast4Doors = cars.Where(c => c.NumberOfDoors >= 4)
+                                  .Where(c => c.Make.Contains("Mazda"));
+
+//foreach (var car in mazdasWithAtLeast4Doors)
+//{
+//    Console.WriteLine($"{car.Make} - {car.Model} - {car.NumberOfDoors}".PadLeft(30));
+//}
+
+// Print  Make + Model for all Makes that starts with M using a Select (projection), and ForEach of a list
+cars.Where(c => c.Make.StartsWith("M"))
+    .Select(c => $"{c.Make} - {c.Model}")
+    .ToList()
+    //.ForEach(c => Console.WriteLine(c))
+    ;
+
+// Example using my extension method of ForEach
+cars.Where(c => c.Make.StartsWith("M"))
+    .Select(c => $"{c.Make} - {c.Model}")
+    //.ForEach(c => Console.WriteLine(c))
+    ;
+
+// Display a List of the 10 most powerful cars (in terms of HP)
+cars.OrderByDescending(c => c.HP)
+    .Take(10)
+    .Select(c => $"{c.HP} - {c.Make} - {c.Model}")
+    //.ForEach(c => Console.WriteLine(c))
+    ;
+
+// /////////////////////////////////////////////////////////////////
+// GroupBy
+
+/// Display the number of models per make that appeared after 1995
+cars.Where(c => c.Year > 1995)
+    .GroupBy(c => c.Make)
+    .Select(c => new
+    {
+        Make = c.Key,
+        Count = c.Count(),
+    })
+    //.ForEach(c => Console.WriteLine($"{c.Make} - {c.Count}"))
+    ;
+
+// or we could project it the same way as the others
+cars.Where(c => c.Year > 1995)
+    .GroupBy(c => c.Make)
+    .Select(c => $"{c.Key} - {c.Count()}")
+    //.ForEach(c => Console.WriteLine(c))
+    ;
+
+/// Display the number of models per make that appeared after 1995 and
+// Display cero if there is no models after that year
+// The trick is to move the Where clause inside the select clause where we have
+// each group available (Note: IGrouping is an IEnumerable with a Key).
+cars.GroupBy(c => c.Make)
+    .Select(g => $"{g.Key} - {g.Where(c => c.Year > 1995).Count()}")
+    //.ForEach(c => Console.WriteLine(c))
+    ;
+
+// The Count method has an overload that takes a function to filter,
+// so we can simplify the g.Where.Count
+cars.GroupBy(c => c.Make)
+    .Select(g => $"{g.Key} - {g.Count(c => c.Year > 1995)}")
+    //.ForEach(c => Console.WriteLine(c))
+    ;
+
+/// Display a list of Makes that have at least 10 models that appeared after 2000
+cars.GroupBy(c => c.Make)
+    .Where(g => g.Where(c => c.Year > 2000).DistinctBy(c => c.Model).Count() > 10)
+    //.ForEach(g => Console.WriteLine($"{g.Key}"))
+    ;
+
+/// Display a list of Makes that have at least 2 models with >= 400 HP
+cars.GroupBy(c => c.Make)
+    .Where(g => g.Where(c => c.HP >= 400).DistinctBy(c => c.Model).Count() > 5)
+    //.ForEach(g => Console.WriteLine($"{g.Key}"))
+    ;
+
+// Another way to do it
+cars.Where(c => c.HP >= 400)
+    .GroupBy(c => c.Make)
+    .Select(g => new { Make = g.Key, NumberOfPowerfulCars = g.DistinctBy(c => c.Model).Count() })
+    .Where(a => a.NumberOfPowerfulCars > 5)
+    //.ForEach(a => Console.WriteLine($"{a.Make}"))
+    ;
+
+// Display the average HP per make
+cars.GroupBy(c => c.Make)
+    //.ForEach(g => Console.WriteLine($"{g.Key} - {g.Average(c => c.HP)}"))
+    ;
+
+// Another way to do it
+cars.GroupBy(c => c.Make)
+    .Select(g => new { Make = g.Key, AverageHP = g.Average(c => c.HP) })
+    //.ForEach(a => Console.WriteLine($"{a.Make} - {a.AverageHP}"))
+    ;
+
+// How many makes build cars with HP 0..100, 101..200, 201..300, 301..400, 401..500
+cars.GroupBy(c => c.HP switch
+{
+    <= 100 => "0..100",
+    <= 200 => "101..200",
+    <= 300 => "201..300",
+    <= 400 => "301..400",
+    _ => "401..500"
+}).OrderBy(g => g.Key)
+  //.ForEach(g => Console.WriteLine($"{g.Key} - {g.Select(c => c.Make).Distinct().Count()}"))
+  .ForEach(g => Console.WriteLine($"{g.Key} - {g.DistinctBy(c => c.Make).Count()}"))
+  ;
+
+Console.WriteLine();
+
+// Otra forma
+cars.GroupBy(c => c.HP switch
+{
+    <= 100 => "0..100",
+    <= 200 => "101..200",
+    <= 300 => "201..300",
+    <= 400 => "301..400",
+    _ => "401..500"
+}).OrderBy(g => g.Key)
+  .Select(g => new {g.Key, Makes = g.GroupBy(c => c.Make).Count()})
+  .ForEach(a => Console.WriteLine($"{a.Key} - {a.Makes}"));
+
+class CarData
+{
+    // We need to let .net the name of the property in the json
+    // We can do it using the JsonPropertyNameAttribute from System.Text.Json.Serialization
+    [JsonPropertyName("id")]
+    public int ID { get; set; }
+    [JsonPropertyName("car_make")]
+    public string Make { get; set; } = string.Empty;
+    [JsonPropertyName("car_model")]
+    public string Model { get; set; } = string.Empty;
+    [JsonPropertyName("car_year")]
+    public int Year { get; set; }
+    [JsonPropertyName("number_of_doors")]
+    public int NumberOfDoors { get; set; }
+    [JsonPropertyName("hp")]
+    public int HP { get; set; }
+
+}
+
+// This was written by me. Is a simple extension method to add ForEach functionality to IEnumerable
+static class EnumerableExt
+{
+    public static void ForEach<T>(this IEnumerable<T> e, Action<T> action)
+    {
+        foreach (var item in e)
+        {
+            action(item);
+        }
+    }
+}
