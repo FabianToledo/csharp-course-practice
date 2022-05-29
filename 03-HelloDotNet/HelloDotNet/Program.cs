4b811700e36f8268acef9617518bb099c8384804
Fabian Toledo <fabian.alejandro.toledo@gmail.com>
2022-05-29 13:12:45 -0300

    Hello dotnet

---
+++ 03-HelloDotNet/HelloDotNet/Program.cs
@@ -0,0 +1,4 @@
+﻿using MathLibrary;
+
+Console.WriteLine($"The answer is: {MathUtil.Add(1,2)}");
+
---
+++ 03-HelloDotNet/MathLibrary/MathUtil.cs
@@ -0,0 +1,12 @@
+﻿namespace MathLibrary
+{
+    public static class MathUtil
+    {
+        public static int Add(int x, int y) => x + y;
+        public static int Subtract(int x, int y) => x - y;
+        public static int Multiply(int x, int y) => x * y;
+        public static int Divide(int x, int y) => x / y;
+ 
+
+    }
+}
