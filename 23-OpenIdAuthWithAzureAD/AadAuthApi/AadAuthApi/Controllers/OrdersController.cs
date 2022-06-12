c96a11e3c0a795e927b49f872dcff88866354917
Fabian Toledo <fabian.alejandro.toledo@gmail.com>
2022-06-12 12:10:05 -0300

    Add a simple endpoint to retrieve data.

---
+++ 23-OpenIdAuthWithAzureAD/AadAuthApi/AadAuthApi/Controllers/OrdersController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace AadAuthApi.Controllers;
+[Route("api/[controller]")]
+[ApiController]
+public class OrdersController : ControllerBase
+{
+    [HttpGet]
+    public IActionResult GetAllOrders()
+    {
+        return Ok(
+            new[] { "Order 1", "Order 2", "Order 3" }
+        );
+    }
+
+
+}
