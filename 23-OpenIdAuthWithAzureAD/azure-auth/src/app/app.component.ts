7101ea6112b5c830c0fdb42f97007e323ecac5d5
Fabian Toledo <fabian.alejandro.toledo@gmail.com>
2022-06-12 09:43:33 -0300

    Once authenticated, use MSGraph to retrieve our profile data and profile data of other users on azure AD.

--- 23-OpenIdAuthWithAzureAD/azure-auth/src/app/app.component.ts
+++ 23-OpenIdAuthWithAzureAD/azure-auth/src/app/app.component.ts
@@ -1,5 +1,14 @@
+import { HttpClient, HttpParams } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 import { MsalService } from '@azure/msal-angular';
+import * as MicrosoftGraph from '@microsoft/microsoft-graph-types'
+
+// defining this interface we are saying that the response
+// from the all users endpoint ('https://graph.microsoft.com/v1.0/users')
+// has a field value of type T (see getUsers())
+interface IODataResult<T> {
+  value: T;
+}
 
 @Component({
   selector: 'app-root',
@@ -8,9 +17,13 @@ import { MsalService } from '@azure/msal-angular';
 })
 export class AppComponent implements OnInit {
   loggedIn = false;
+  profile?: MicrosoftGraph.User;
+  users?: MicrosoftGraph.User[];
+  textToFilter = "" ;
 
-  constructor(private authService: MsalService) {}
-
+  constructor(private authService: MsalService,
+    private client: HttpClient) {}
+    
   ngOnInit(): void {
     this.checkAccount();
   }
@@ -30,4 +43,30 @@ export class AppComponent implements OnInit {
   logout() {
     this.authService.logout();
   }
+
+  getProfile() {
+    this.client.get('https://graph.microsoft.com/v1.0/me/')
+      .subscribe(response => this.profile = response);
+  }
+
+  // Query MSGraph using **OData** standard
+  getUsers() {
+    let params = new HttpParams();
+    //params = params.set("$top", "10"); // This will get only top 10 results
+    if(this.textToFilter)
+    {
+      params = params.set(
+        "$filter",
+        `startsWith(displayName, '${this.textToFilter}')`,
+      );
+    }
+
+    let url = `https://graph.microsoft.com/v1.0/users?${params.toString()}`;
+    this.client
+      // Two ways to getting the data from the field value of the response:
+      .get<IODataResult<MicrosoftGraph.User[]>>(url) // Define an interface with the field value
+      // .get<any>(url) // or say that the response is <any>
+      .subscribe(response => this.users = response.value)
+  }
+
 }
